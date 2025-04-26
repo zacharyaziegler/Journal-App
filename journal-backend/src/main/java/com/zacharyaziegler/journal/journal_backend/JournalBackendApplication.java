dfcc4d854ee1895ab1ccfb70d2305bbaa67dcb87
zacharyaziegler <zacharyaziegler@gmail.com>
2025-04-26 16:25:20 -0400

    Initial commit

---
+++ journal-backend/src/main/java/com/zacharyaziegler/journal/journal_backend/JournalBackendApplication.java
@@ -0,0 +1,13 @@
+package com.zacharyaziegler.journal.journal_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JournalBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JournalBackendApplication.class, args);
+	}
+
+}
---
+++ journal-backend/src/test/java/com/zacharyaziegler/journal/journal_backend/JournalBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zacharyaziegler.journal.journal_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JournalBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
