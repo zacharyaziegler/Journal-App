b2d82854db01f8d69c40a0df791bb5fe1b55390b
zacharyaziegler <zacharyaziegler@gmail.com>
2025-04-29 16:53:42 -0400

    Implemented add journal entry feature

--- journal-frontend/src/App.jsx
+++ journal-frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import EntryDetail from "./pages/EntryDetail";
-// import AddEdit from "./pages/AddEdit"
+import AddEdit from "./pages/AddEdit"
 // import EntryDetail from "./pages/EntryDetail"
 import "./styles/App.css";
 
@@ -11,7 +11,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/entries/:id" element={<EntryDetail />} />
-        {/* <Route path="/add" element={<AddEdit />} /> */}
+        <Route path="/add" element={<AddEdit />} />
         {/* <Route path="/edit/:id" element={<AddEdit />} /> */}
         {/* <Route path="/entries/:id" element={<EntryDetail />} /> */}
       </Routes>
